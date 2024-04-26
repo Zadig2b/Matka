860f4fcb61ddc35b6e4a5f434bcbaa4e9e1ca971
romain <romain.dugeay@gmail.com>
2024-04-26 13:54:37 +0200

    deleted travelfetch route

--- frontend/src/app/[travel]/page.jsx
+++ frontend/src/app/[travel]/page.jsx
@@ -1,12 +1,47 @@
-"use client"
+"use client";
 
-export default function TravelDetails(props){
-    console.log(props);
-    return(
-        <div>
-            <h1>Travel Details</h1>
-            <p>Travel Name: {props.params.travel}</p>
-        </div>
-        
-    )
+import { useEffect, useState } from "react";
+import Navbar from "@/components/navbar/Navbar";
+import TravelCard from "@/components/travelCard/TravelCard";
+
+export default function CharacterDetail(props) {
+  // Initialisation des états pour gérer le chargement, les erreurs, et les données reçues.
+  const [loading, setLoading] = useState(true); // État de chargement des données.
+  const [error, setError] = useState(false); // État pour capturer une éventuelle erreur lors du fetch.
+  const [data, setData] = useState(null); // Stockage des données reçues du fetch.
+
+  useEffect(() => {
+    // Déclenchement de la récupération des données de personnages au montage du composant.
+    try {
+      fetch(
+        "https://rickandmortyapi.com/api/character/" + props.params.travel
+      )
+        .then((response) => response.json()) // Transformation de la réponse en JSON.
+        .then((data) => {
+          setLoading(false); // Arrêt de l'indicateur de chargement après la réception des données.
+          setData(data); // Enregistrement des données reçues dans l'état 'data'.
+        });
+    } catch (error) {
+      setError(true); // Enregistrement de l'erreur dans l'état 'error'.
+      setLoading(false); // Arrêt de l'indicateur de chargement en cas d'erreur.
+    }
+  }, []); // Le tableau vide indique que cet effet ne s'exécute qu'au montage.
+
+  return (
+    <main>
+      <Navbar />
+      {/* Affichage conditionnel en fonction de l'état du chargement et des erreurs */}
+      {loading && !error && <div>Données en cours de chargement !</div>}
+      {!loading && !error && data && (
+        <TravelCard
+          name={data.name}
+          species={data.species}
+          gender={data.gender}
+          origin={data.origin.name}
+          image={data.image}
+        />
+      )}
+      {!loading && error && <div>Une erreur est survenue</div>}
+    </main>
+  );
 }
